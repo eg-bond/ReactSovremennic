c77d7868e75751d28e03deade4ce13c36c52e7b2
eg_bond <eg-bond@yandex.ru>
2020-03-09 10:49:09 +0300

    Updates

---
+++ precache-manifest.cb6edabfae64a74d7c0b13a64d2128ff.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d118ebc0e9f910158a08a259e7d15a55",
+    "url": "/ReactSovremennic/index.html"
+  },
+  {
+    "revision": "f41ab02b75af85aad5e7",
+    "url": "/ReactSovremennic/static/css/2.5777a307.chunk.css"
+  },
+  {
+    "revision": "9934b6688fe13dc81a27",
+    "url": "/ReactSovremennic/static/css/main.9a581ff4.chunk.css"
+  },
+  {
+    "revision": "f41ab02b75af85aad5e7",
+    "url": "/ReactSovremennic/static/js/2.3f4814c3.chunk.js"
+  },
+  {
+    "revision": "9934b6688fe13dc81a27",
+    "url": "/ReactSovremennic/static/js/main.79fe44de.chunk.js"
+  },
+  {
+    "revision": "d3b542feb7f03e4065b9",
+    "url": "/ReactSovremennic/static/js/runtime-main.cadd256d.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/ReactSovremennic/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/ReactSovremennic/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/ReactSovremennic/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/ReactSovremennic/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/ReactSovremennic/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
