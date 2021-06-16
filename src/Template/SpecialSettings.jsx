28b72600de860a4e9d691aa60e530c7be6373532
eg-bond <eg-bond@yandex.ru>
2021-06-16 08:38:30 +0300

    special settings profiled and refactored

--- src/Template/SpecialSettings.jsx
+++ src/Template/SpecialSettings.jsx
@@ -1,10 +1,4 @@
-import React from 'react'
-import Switch from '@material-ui/core/Switch'
-import ButtonGroup from '@material-ui/core/ButtonGroup'
-import Button from '@material-ui/core/Button'
-import IconButton from '@material-ui/core/IconButton'
-import RemoveRedEyeOutlinedIcon from '@material-ui/icons/RemoveRedEyeOutlined'
-import Brightness1Icon from '@material-ui/icons/Brightness1'
+import React, { useEffect } from 'react'
 import {
   switchSiteMode,
   switchSiteTheme,
@@ -13,34 +7,12 @@ import {
 } from '../REDUX/specialReduser'
 import { compose } from 'redux'
 import { connect } from 'react-redux'
-
-const SiteModeButton = ({ siteMode, switchSiteMode, fontSize }) => {
-  let modeToDispatch = siteMode === 'default' ? 'special' : 'default'
-  const siteModeHandler = () => {
-    if (fontSize !== '14px') {
-      switchFontSize('14px')
-    }
-    switchSiteMode(modeToDispatch)
-  }
-
-  return (
-    <Button
-      className='specialSettings__modeButton'
-      onClick={() => siteModeHandler()}
-      variant='contained'
-      startIcon={<RemoveRedEyeOutlinedIcon />}>
-      {siteMode === 'default'
-        ? 'Версия для слабовидящих'
-        : 'Обычная версия сайта'}
-    </Button>
-  )
-}
-
-const ThemeButton = React.memo(({ theme, cl, switchSiteTheme }) => (
-  <IconButton onClick={() => switchSiteTheme(theme)} className={cl}>
-    <Brightness1Icon />
-  </IconButton>
-))
+import {
+  FontButtons,
+  ImgSwitcher,
+  SiteModeButton,
+  ThemeButtons,
+} from './SpecialSettingsComp'
 
 function SpecialSettings({
   switchSiteTheme,
@@ -52,17 +24,16 @@ function SpecialSettings({
   theme,
   fontSize,
 }) {
-  const handleImgSwitch = () =>
-    imgHidden ? switchImagesVisibility(false) : switchImagesVisibility(true)
+  useEffect(() => {
+    if (fontSize !== '14px' && siteMode === 'default') {
+      switchFontSize('14px')
+    }
+  }, [siteMode])
 
   if (siteMode === 'default') {
     return (
       <div className={`space`}>
-        <SiteModeButton
-          siteMode={siteMode}
-          switchSiteMode={switchSiteMode}
-          fontSize={fontSize}
-        />
+        <SiteModeButton siteMode={siteMode} switchSiteMode={switchSiteMode} />
       </div>
     )
   }
@@ -70,68 +41,17 @@ function SpecialSettings({
   return (
     <div className={`specialSettings__container 'theme__${theme}__borders'`}>
       <div className={`specialSettings__flex`}>
-        <div className='specialSettings__flex__item'>
-          <div className={'specialSettings__flex__title'}>ЦВЕТОВАЯ СХЕМА</div>
-          <ThemeButton
-            theme='blackWhite'
-            cl='themeBW'
-            switchSiteTheme={switchSiteTheme}
-          />
-          <ThemeButton
-            theme='whiteBlack'
-            cl='themeWB'
-            switchSiteTheme={switchSiteTheme}
-          />
-          <ThemeButton
-            theme='blackRed'
-            cl='themeBR'
-            switchSiteTheme={switchSiteTheme}
-          />
-          <ThemeButton
-            theme='yellowBrown'
-            cl='themeYB'
-            switchSiteTheme={switchSiteTheme}
-          />
-          <ThemeButton
-            theme='blueGreen'
-            cl='themeBG'
-            switchSiteTheme={switchSiteTheme}
-          />
-        </div>
+        <ThemeButtons switchSiteTheme={switchSiteTheme} />
 
-        <div className={`specialSettings__flex__item`}>
-          <div className={'specialSettings__flex__title'}>РАЗМЕР ШРИФТА</div>
-          <ButtonGroup
-            className={'specialSettings__flex__fontButtons'}
-            size='large'
-            variant='contained'
-            aria-label='contained primary button group'>
-            <Button onClick={() => switchFontSize('14px')}>100%</Button>
-            <Button onClick={() => switchFontSize('21px')}>150%</Button>
-            <Button onClick={() => switchFontSize('26px')}>200%</Button>
-          </ButtonGroup>
-        </div>
+        <FontButtons switchFontSize={switchFontSize} />
 
-        <div className='specialSettings__flex__item'>
-          <div className={'specialSettings__flex__title'}>ИЗОБРАЖЕНИЯ</div>
-          <span className={'specialSettings__switchOff'}>Выкл.</span>
-          <Switch
-            onClick={handleImgSwitch}
-            checked={!imgHidden}
-            onKeyPress={e => e.key === 'Enter' && handleImgSwitch()}
-            color='default'
-            name='imgSwitcher'
-            inputProps={{ 'aria-label': 'primary checkbox' }}
-          />
-          <span className={'specialSettings__switchOn'}>Вкл.</span>
-        </div>
+        <ImgSwitcher
+          imgHidden={imgHidden}
+          switchImagesVisibility={switchImagesVisibility}
+        />
 
         <div className='specialSettings__flex__item specialSettings__flex__modeButton'>
-          <SiteModeButton
-            siteMode={siteMode}
-            switchSiteMode={switchSiteMode}
-            fontSize={fontSize}
-          />
+          <SiteModeButton siteMode={siteMode} switchSiteMode={switchSiteMode} />
         </div>
       </div>
     </div>
