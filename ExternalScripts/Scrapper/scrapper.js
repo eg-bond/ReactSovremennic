41873cf9128feb03a4f4a320c7ecb8e71f082dce
eg-bond <eg-bond@yandex.ru>
2022-01-29 14:59:44 +0300

    last 29-01-22

--- ExternalScripts/Scrapper/scrapper.js
+++ ExternalScripts/Scrapper/scrapper.js
@@ -3,7 +3,7 @@ import cherio from 'cherio'
 import { getPageContent } from './puppeteer.js'
 
 const SITE = 'https://www.kinopoisk.ru/film/'
-const cinemaIds = ['4290071', '988816', '468373']
+const cinemaIds = ['1103803', '1234853']
 
 function capitalizeFirstLetter(string) {
   return string.charAt(0).toUpperCase() + string.slice(1)
--- src/App.js
+++ src/App.js
@@ -16,6 +16,8 @@ import { switchSiteMode, switchFontSize } from './REDUX/specialReduser'
 import { changeAppColors, modifiedClass, queries } from './helpers'
 import { useMediaQuery } from '@material-ui/core'
 import Content from './Content/Content'
+import CovidMessage from './Template/CovidMessage'
+import { Route } from 'react-router-dom'
 
 const App = ({
   createActualDatesArr,
@@ -80,6 +82,8 @@ const App = ({
         <div className='separatorMobile' />
 
         <div className={`container wrapper ${imgHidden ? 'hideImages' : ''}`}>
+          <CovidMessage />
+
           {siteMode === 'default' && (
             <FilmSwiper films={films} mobile={mobileQ} />
           )}
