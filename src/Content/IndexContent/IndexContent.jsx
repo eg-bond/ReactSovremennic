523e561ce6b8fe348af1f770ecf6a16f601c6975
eg-bond <eg-bond@yandex.ru>
2022-04-23 12:29:27 +0300

    last 23-04-22

--- src/Content/IndexContent/IndexContent.jsx
+++ src/Content/IndexContent/IndexContent.jsx
@@ -16,7 +16,7 @@ function IndexContent({ siteMode, films, mobileQ, desktopQ }) {
             <LiteYouTubeEmbed
               id='PLfrc7TOjXulMiQWLR1Jr8ZiZ2b7hWqSZf'
               playlist={true}
-              playlistCoverId='14Ek06Rfn90'
+              playlistCoverId='Y5nkcOuHL54'
               title='index_trailers_playlist'
               poster='maxresdefault'
             />
