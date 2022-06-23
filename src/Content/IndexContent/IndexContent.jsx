fe087fa8b174f2844552e77462d4126c254e0796
eg-bond <eg-bond@yandex.ru>
2022-06-23 12:07:17 +0300

    main image changed

--- src/Content/IndexContent/IndexContent.jsx
+++ src/Content/IndexContent/IndexContent.jsx
@@ -16,7 +16,7 @@ function IndexContent({ siteMode, films, mobileQ, desktopQ }) {
             <LiteYouTubeEmbed
               id='PLfrc7TOjXulMiQWLR1Jr8ZiZ2b7hWqSZf'
               playlist={true}
-              playlistCoverId='zbN4ychwEnw'
+              playlistCoverId='V8NkDZjMBxQ'
               title='index_trailers_playlist'
               poster='maxresdefault'
             />
