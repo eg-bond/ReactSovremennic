e484dca3b830d2d0c8c02df91102f3cb097d4f47
eg-bond <eg-bond@yandex.ru>
2022-07-27 13:47:43 +0300

    last 27-07-22

--- src/REDUX/seansReduser.js
+++ src/REDUX/seansReduser.js
@@ -4,16 +4,16 @@ const CREATE_ACTUAL_DATES_ARR = 'CREATE_ACTUAL_DATES_ARR'
 
 let initialState = {
   datesArr: [
-    ['day0', 'Воскресенье', '24 июля'],
+    ['day0', 'Воскресенье', '31 июля'],
     ['day1', 'Понедельник', '25 июля'],
     ['day2', 'Вторник', '26 июля'],
     ['day3', 'Среда', '27 июля'],
-    ['day4', 'Четверг', '21 июля'],
-    ['day5', 'Пятница', '22 июля'],
-    ['day6', 'Суббота', '23 июля'],
+    ['day4', 'Четверг', '28 июля'],
+    ['day5', 'Пятница', '29 июля'],
+    ['day6', 'Суббота', '30 июля'],
   ],
   actualDatesArr: [], // массив с датами, расположенными в правильном порядке
-  weekStartsFrom: 'th', // monday либо любое другое значение
+  weekStartsFrom: 'monday', // monday либо любое другое значение
   activeSceduleItemKey: '',
   buttonTitle: null,
 }
