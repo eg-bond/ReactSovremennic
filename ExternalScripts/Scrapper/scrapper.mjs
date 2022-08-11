00c3b389af1113afdc2330bcfbcfc71cda0c9a28
eg-bond <eg-bond@yandex.ru>
2022-08-11 21:58:42 +0300

    last 11-08-22

--- ExternalScripts/Scrapper/scrapper.mjs
+++ ExternalScripts/Scrapper/scrapper.mjs
@@ -3,7 +3,7 @@ import cherio from 'cherio'
 import { getPageContent } from './puppeteer.mjs'
 
 const SITE = 'https://www.kinopoisk.ru/film/'
-const cinemaIds = ['1311146']
+const cinemaIds = ['4518842', '1243552']
 
 function capitalizeFirstLetter(string) {
   return string.charAt(0).toUpperCase() + string.slice(1)
