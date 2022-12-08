b1443427f65a77369df720d0f87e7423cc4cddc8
eg-bond <eg-bond@yandex.ru>
2022-12-08 11:11:36 +0300

    last 08-12-22

--- src/REDUX/cinemaReduser.js
+++ src/REDUX/cinemaReduser.js
@@ -6,7 +6,7 @@ let initialState = {
   films: filmsArray,
   filmsObject: null,
   filmsToday: [],
-  filmsTodayAmount: 3,
+  filmsTodayAmount: 6,
 }
 
 export const cinemaReduser = (state = initialState, action) => {
