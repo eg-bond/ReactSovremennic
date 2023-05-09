d2a7360c5ad45a2407b2ff81ef52b0c6503315e5
eg-bond <eg-bond@yandex.ru>
2023-05-09 23:47:56 +0300

    last 09-05-23

--- src/REDUX/seansReduser.js
+++ src/REDUX/seansReduser.js
@@ -8,12 +8,12 @@ let initialState = {
     ['day1', 'Понедельник', '8 мая'],
     ['day2', 'Вторник', '9 мая'],
     ['day3', 'Среда', '10 мая'],
-    ['day4', 'Четверг', '4 мая'],
-    ['day5', 'Пятница', '5 мая'],
+    ['day4', 'Четверг', '11 мая'],
+    ['day5', 'Пятница', '12 мая'],
     ['day6', 'Суббота', '6 мая'],
   ],
   actualDatesArr: [], // массив с датами, расположенными в правильном порядке
-  weekStartsFrom: 'th', // monday либо любое другое значение
+  weekStartsFrom: 'monday', // monday либо любое другое значение
   activeSceduleItemKey: '',
   buttonTitle: null,
 }
@@ -38,8 +38,11 @@ export const seansReduser = (state = initialState, action) => {
     case CREATE_ACTUAL_DATES_ARR:
       let newArr = []
       if (state.weekStartsFrom === 'monday') {
-        newArr = [...state.datesArr, state.datesArr[0]]
-        newArr.shift()
+        // newArr = [...state.datesArr, state.datesArr[0]]
+        // newArr.shift()
+
+        newArr = [state.datesArr[6], ...state.datesArr]
+        newArr.pop()
       } else {
         newArr = [
           ...state.datesArr,
