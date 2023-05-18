0ac465d6b56fa52453b4229e84f4eab74732bf4f
eg-bond <eg-bond@yandex.ru>
2023-05-18 23:38:54 +0300

    bar swiper in prog

--- src/Template/BarSwiper.tsx
+++ src/Template/BarSwiper.tsx
@@ -1,7 +1,8 @@
 import SwiperCore, { Autoplay } from 'swiper'
 import { Swiper, SwiperSlide } from 'swiper/react'
+import { Splide, SplideSlide } from '@splidejs/react-splide'
 
-SwiperCore.use([Autoplay])
+// SwiperCore.use([Autoplay])
 
 const barSwiperSlide = (slideKey: string) => {
   return (
@@ -12,22 +13,46 @@ const barSwiperSlide = (slideKey: string) => {
     </SwiperSlide>
   )
 }
+// const barSwiperSlide = (slideKey: string) => {
+//   return (
+//     <SwiperSlide key={slideKey}>
+//       <div className='imgContainer skeleton'>
+//         <img src={`./Images/${slideKey}.webp`} alt={slideKey} />
+//       </div>
+//     </SwiperSlide>
+//   )
+// }
 
 const slideKeys = ['combo2', 'combo1']
 
 const BarSwiper = () => {
   return (
-    <Swiper
+    // <Swiper
+    //   className='swiper-container-bar'
+    //   loop={true}
+    //   autoplay={{
+    //     delay: 4000,
+    //     disableOnInteraction: false,
+    //     reverseDirection: true,
+    //     pauseOnMouseEnter: true,
+    //   }}>
+    //   {slideKeys.map(barSwiperSlide)}
+    // </Swiper>
+    <Splide
       className='swiper-container-bar'
-      loop={true}
-      autoplay={{
-        delay: 4000,
-        disableOnInteraction: false,
-        reverseDirection: true,
-        pauseOnMouseEnter: true,
+      options={{
+        perPage: 1,
+        perMove: 1,
+        pagination: false,
+        arrows: false,
+        type: 'loop',
+        autoplay: true,
+        interval: 3500,
+        pauseOnHover: true,
+        pauseOnFocus: true,
       }}>
       {slideKeys.map(barSwiperSlide)}
-    </Swiper>
+    </Splide>
   )
 }
 export default BarSwiper
