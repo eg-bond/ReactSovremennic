ce6c91a1c1df10098056c75e564a5a1491e21123
eg-bond <eg-bond@yandex.ru>
2023-05-27 11:08:22 +0300

    excel macros to TS

--- ExternalScripts/Excel/insertSceduleFromXLSX.ts
+++ ExternalScripts/Excel/insertSceduleFromXLSX.ts
@@ -1,73 +1,57 @@
 import xlsx from 'xlsx'
-// import fs from 'fs'
+import fs from 'fs'
 import { filmsArray } from '../../src/REDUX/filmsArray.ts'
 
-//Вытаскиваем JS объект с расписанием из екселя
-const workBook = xlsx.readFile('ExternalScripts/Excel/macros.xlsm')
-const ws = workBook.Sheets['Лист2']
-const data = xlsx.utils.sheet_to_json(ws) as dataT
-
-type dataSingleObj = {
+type dataT = Array<{
   day: 'day0' | 'day1' | 'day2' | 'day3' | 'day4' | 'day5' | 'day6'
   time: 'string'
   filmTitle: 'string'
   cost: number
-}
-type dataT = Array<dataSingleObj>
+}>
 
-//Преобразуем извлеченный из екселя объект в удобный для дальнейшего использования
-const scedule = dataToObj(data)
+// retrieve data from excel file
+const workBook = xlsx.readFile('ExternalScripts/Excel/macros.xlsm')
+const ws = workBook.Sheets['Лист2']
+const data = xlsx.utils.sheet_to_json(ws) as dataT
 
-// type sceduleT = {
-//   [`day1`]: {
-//     time: 'string'
-//     filmTitle: 'string'
-//     cost: number
-//   }
-// }
+const schedule = dataToObj(data)
+const finalSchedule = finalizeScedule(schedule)
 
+// Write final data to schedule.ts file
+const finalData = `let schedule = ${JSON.stringify(finalSchedule)}
+export default schedule;`
+fs.writeFileSync('src/Content/Seance/schedule.ts', finalData)
+
+//------------------------------------------------------------------------------------
 function dataToObj(data: dataT) {
-  // const scedule = {}
-  const scedule = {} as {
+  const schedule = {} as {
     [index: string]: Array<[string, string, number]>
   }
 
   for (let i = 0; i < 7; i++) {
-    const dayArr = data.filter(item => item.day === `day${i}`)
-    scedule[`day${i}`] = dayArr.map(item => [
+    const currentKey = `day${i}` as `day${0 | 1 | 2 | 3 | 4 | 5 | 6}`
+    const dayArr = data.filter(item => item.day === currentKey)
+    schedule[currentKey] = dayArr.map(item => [
       item.time,
       item.filmTitle,
       item.cost,
     ])
   }
 
-  return scedule
+  return schedule
 }
 
-const finalScedule = finalizeScedule(scedule)
+// Replace titles with ones from filmsArray and add age to final schedule
+function finalizeScedule(schedule: ReturnType<typeof dataToObj>) {
+  const actualTitleAndAge = findTitleAndAge()
 
-console.log(finalScedule)
-
-//Формируем данные для записи в файл расписания в строковом формате
-// const finalData = `let scedule = ${JSON.stringify(finalScedule)}
-// export default scedule;`
-
-// //Записываем получившиеся данные в файл
-// fs.writeFileSync('src/Content/Seance/scedule.ts', finalData)
-
-// Актуализирует названия фильмов в расписании в соответствии с названиями из 'filmsArray'
-// и добавляет возрастное ограничение
-function finalizeScedule(scedule: ReturnType<typeof dataToObj>) {
-  const sceduleCopy: typeof scedule = JSON.parse(JSON.stringify(scedule))
-  const actualTitleAndAge = {}
-
-  filmsArray.forEach(filmItem => {
-    const key = findFirstWordInTitle(filmItem.title)
-    actualTitleAndAge[key] = [filmItem.title + ' 2D', filmItem.age]
-  })
-
-  Object.keys(scedule).forEach(key => {
-    const updatedDay = scedule[key].map(seance => {
+  const finalSchedule = {} as {
+    [index: string]: Array<[string, string, string, number | string]>
+  }
+  // loop through all days
+  Object.keys(schedule).forEach(key => {
+    // map through all seance in a day an add actual title and age
+    const updatedDay = schedule[key].map(seance => {
       const shortTitle = findFirstWordInTitle(seance[1])
 
       if (actualTitleAndAge[shortTitle]) {
@@ -82,13 +66,23 @@ function finalizeScedule(scedule: ReturnType<typeof dataToObj>) {
       }
     })
     //@ts-ignore
-    sceduleCopy[key] = updatedDay
+    finalSchedule[key] = updatedDay
   })
 
-  return sceduleCopy
+  return finalSchedule
 }
 
-// function normalizeNames(scedule: ReturnType<typeof dataToObj>) {}
+// retrieve title and age of all items in filmsArray
+function findTitleAndAge() {
+  const actualTitleAndAge = {} as { [index: string]: [string, string] }
+
+  filmsArray.forEach(filmItem => {
+    const key = findFirstWordInTitle(filmItem.title)
+    actualTitleAndAge[key] = [filmItem.title + ' 2D', filmItem.age]
+  })
+
+  return actualTitleAndAge
+}
 
 function findFirstWordInTitle(title: string) {
   const match = title.match(/^\S+/gi) || ['']
