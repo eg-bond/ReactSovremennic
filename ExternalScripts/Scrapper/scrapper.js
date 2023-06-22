eb636bab39345c4395338912839e0d4aaf2aefdb
eg-bond <eg-bond@yandex.ru>
2023-06-22 20:52:01 +0300

    last 22-06-23

--- ExternalScripts/Scrapper/scrapper.js
+++ ExternalScripts/Scrapper/scrapper.js
@@ -3,7 +3,7 @@ import { getPageContent } from './puppeteer.js'
 import { JSDOM } from 'jsdom'
 
 const URL = 'https://www.kinopoisk.ru/film/'
-const cinemaIds = ['999881']
+const cinemaIds = ['4458585']
 
 parseCinemaData(cinemaIds)
 
