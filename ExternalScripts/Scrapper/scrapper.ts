3f4d52e4988b896a5bf749a0166326c92d667c2d
eg-bond <eg-bond@yandex.ru>
2023-08-23 11:51:30 +0300

    last 23-08-23

--- ExternalScripts/Scrapper/scrapper.ts
+++ ExternalScripts/Scrapper/scrapper.ts
@@ -3,7 +3,7 @@ import { getPageContent } from './puppeteer.ts'
 import { JSDOM } from 'jsdom'
 
 const URL = 'https://www.kinopoisk.ru/film/'
-const cinemaIds = ['4987446']
+const cinemaIds = ['468846', '5091596', '478052']
 
 parseCinemaData(cinemaIds)
 //------------------------------------------------------------------------------
