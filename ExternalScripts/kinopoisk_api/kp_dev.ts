37ef43781f29f645082abec65992b0458e83b22a
eg-bond <eg-bond@yandex.ru>
2023-12-06 13:53:31 +0300

    last 06-12-23

--- ExternalScripts/kinopoisk_api/kp_dev.ts
+++ ExternalScripts/kinopoisk_api/kp_dev.ts
@@ -3,7 +3,7 @@ import config from './config.ts'
 const kp = new kpDev.KinopoiskDev(config.KP_DEV_TOKEN)
 
 // needed movie ids
-const ids = [5360443, 1254052]
+const ids = [5324995, 4740371]
 logAllMovies(ids)
 //-----------------------------------------------------------
 
