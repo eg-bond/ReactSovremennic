469bf268ba8402c1afaffdf0486fcb84ad4177bc
eg-bond <eg-bond@yandex.ru>
2024-02-13 22:23:09 +0300

    last 13-02-24

--- src/REDUX/cinema/cinemaReducer.ts
+++ src/REDUX/cinema/cinemaReducer.ts
@@ -6,7 +6,7 @@ const initialState: CinemaStateT = {
   films: filmsArray,
   filmsObject: {},
   filmsToday: [],
-  filmsTodayAmount: 4,
+  filmsTodayAmount: 5,
 }
 
 const cinemaSlice = createSlice({
