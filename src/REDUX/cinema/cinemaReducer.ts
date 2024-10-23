1f32675d7bf44fc4d407c77306e5aa5253eb4af2
eg-bond <eg-bond@yandex.ru>
2024-10-23 12:37:39 +0300

    refactor: new schedule till 27-10-24

--- src/REDUX/cinema/cinemaReducer.ts
+++ src/REDUX/cinema/cinemaReducer.ts
@@ -6,7 +6,7 @@ const initialState: CinemaStateT = {
   films: filmsArray,
   filmsObject: {},
   filmsToday: [],
-  filmsTodayAmount: 7,
+  filmsTodayAmount: 6,
 };
 
 const cinemaSlice = createSlice({
