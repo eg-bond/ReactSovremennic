a0684c51c7a7e00a6613803a4b639e8522843cbf
eg-bond <eg-bond@yandex.ru>
2025-03-03 11:16:45 +0300

    refactor: new movies

--- src/Template/Footer.tsx
+++ src/Template/Footer.tsx
@@ -6,7 +6,7 @@ const Footer = memo(function Footer() {
   return (
     <div className="footer container">
       <Link className="footer__a" to="/" onClick={scrollToTop}>
-        Кинотеатр «Современник» © 2005 - 2023
+        Кинотеатр «Современник» © 2005 - 2025
       </Link>
     </div>
   );
