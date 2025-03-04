7dc2993d5610ab58c0e8f533288658540195ea4d
eg-bond <eg-bond@yandex.ru>
2025-03-04 15:51:30 +0300

    refactor: akvamarin

--- src/Template/IndexAdvXS.tsx
+++ src/Template/IndexAdvXS.tsx
@@ -5,12 +5,23 @@ function IndexAdvXS() {
     <div className="container" data-testid="mobile_adv">
       <div className="indexAdvXS">
         <div className="indexAdvXS__item1">
+          <div className="indexAdvXS__item1__imgCont">
+            <a href="https://vk.com/sushikinno">
+              <img
+                alt="Шоу 7 марта"
+                src="Images/akvamarin.webp"
+                style={{ height: '100%' }}
+              />
+            </a>
+          </div>
+        </div>
+        {/* <div className="indexAdvXS__item1">
           <div className="indexAdvXS__item1__imgCont skeleton">
             <Link to="/sushi">
               <img alt="sushi" src="Images/sushi_adv.webp" />
             </Link>
           </div>
-        </div>
+        </div> */}
 
         <div className="indexAdvXS__item1_2">
           <a className="linkWrapper" href="https://lotten.ru" tabIndex={2}>
