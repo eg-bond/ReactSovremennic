3973df96ada5f91e11ae068013ab2da78cf185d8
eg-bond <eg-bond@yandex.ru>
2025-03-30 14:18:11 +0300

    chore: barrelsby for autocreating reexports added

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,2 +1,7 @@
 export const PRE_SHOW_SERVICE = '(предсеансовое обслуживание)';
 export const PRE_SHOW_SERVICE_SHORT = '(предс. обслуживание)';
+
+export const Y_MAPS_SRC =
+'https://yandex.ru/map-widget/v1/?um=constructor%' +
+'3Ab3cf16ca0bc3eed0838b34be9c0607866b4c270427026b7fe0dd14ef4096116b' +
+'&amp;source=constructor';
