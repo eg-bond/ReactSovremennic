501e2ada0c873c57fc407044238b34ce4afaf3a8
eg-bond <eg-bond@yandex.ru>
2025-05-16 17:41:11 +0300

    refactor: new movies

--- ExternalScripts/kinopoisk_api/kp_dev.ts
+++ ExternalScripts/kinopoisk_api/kp_dev.ts
@@ -5,7 +5,7 @@ const kp = new kpDev.KinopoiskDev(config.KP_DEV_TOKEN);
 
 // needed movie ids
 
-const ids = [5417693, 6440213, 5001443];
+const ids = [5366081, 1047492];
 logAllMovies(ids);
 // -----------------------------------------------------------
 
