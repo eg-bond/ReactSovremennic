7c6e9c86cfec8d22d52c076dcb1d34734ecf9883
eg-bond <eg-bond@yandex.ru>
2026-06-09 15:32:08 +0300

    refactor: new schedule

--- src/REDUX/seance/seanceReducer.ts
+++ src/REDUX/seance/seanceReducer.ts
@@ -3,13 +3,13 @@ import type { DateKeysT, SeanceStateT, getDayT } from './seanceReducerT';
 
 export const initialState: SeanceStateT = {
   datesArr: [
-    ['day4', 'Четверг', '4 июня'],
-    ['day5', 'Пятница', '5 июня'],
-    ['day6', 'Суббота', '6 июня'],
-    ['day0', 'Воскресенье', '7 июня'],
     ['day1', 'Понедельник', '8 июня'],
     ['day2', 'Вторник', '9 июня'],
     ['day3', 'Среда', '10 июня'],
+    ['day4', 'Четверг', '11 июня'],
+    ['day5', 'Пятница', '12 июня'],
+    ['day6', 'Суббота', '13 июня'],
+    ['day0', 'Воскресенье', '14 июня'],
   ],
   activeScheduleItemKey: '',
 };
