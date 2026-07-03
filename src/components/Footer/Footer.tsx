e38eeb225362ac597228fcb4f995ce48bb3a72f8
eg-bond <eg-bond@yandex.ru>
2026-07-03 13:10:59 +0300

    refactor: minor last shtrihs lul

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,7 @@
 import { memo } from 'react';
 import { Link } from 'react-router-dom';
-import { Socials } from './Socials';
 import { scrollToTop } from '@/utils/helpers';
+import { Socials } from './Socials';
 import * as s from './Footer.css';
 
 const Footer = memo(function Footer() {
@@ -9,7 +9,7 @@ const Footer = memo(function Footer() {
     <div className={`${s.footer} container`}>
       <div className={s.footerContent}>
         <Link className={s.footerLink} to="/" onClick={scrollToTop}>
-          © 2005 - 2025, Кинотеатр «Современник»
+          © 2005 - 2026, Кинотеатр «Современник»
         </Link>
         <Socials />
       </div>
