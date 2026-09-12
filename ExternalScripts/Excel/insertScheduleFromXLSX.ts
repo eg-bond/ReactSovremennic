5ad5a61da41a5ad4927cf19febdaa027c13b4fca
eg-bond <eg-bond@yandex.ru>
2026-09-12 23:13:46 +0300

    refactor: new schedule

--- ExternalScripts/Excel/insertScheduleFromXLSX.ts
+++ ExternalScripts/Excel/insertScheduleFromXLSX.ts
@@ -19,13 +19,13 @@ const schedule = dataToObj(data);
 const finalSchedule = finalizeScedule(schedule);
 
 const datesArr: Array<[string, string, string]> = [
-  ['day1', 'Понедельник', '7 сентября'],
-  ['day2', 'Вторник', '8 сентября'],
-  ['day3', 'Среда', '9 сентября'],
   ['day4', 'Четверг', '10 сентября'],
   ['day5', 'Пятница', '11 сентября'],
   ['day6', 'Суббота', '12 сентября'],
   ['day0', 'Воскресенье', '13 сентября'],
+  ['day1', 'Понедельник', '14 сентября'],
+  ['day2', 'Вторник', '15 сентября'],
+  ['day3', 'Среда', '16 сентября'],
 ];
 
 // Write final data to public/schedule.json file
